28066e967c81c3707d40ea17369f0d53dd2ff8d4
JordLoop <JordLoop@gmail.com>
2017-07-14 14:13:14 -0700

    add form to create a new character

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Character } from '../character.model';
+import { CharacterService } from '../character.service'
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.css']
+  styleUrls: ['./admin.component.css'],
+  providers: [ CharacterService ]
 })
-export class AdminComponent implements OnInit {
+export class AdminComponent{
 
-  constructor() { }
-
-  ngOnInit() {
+  submitForm( playerName: string, characterName: string, characterClass: number, level: number, background: string, race: string, alignment: string ) {
+  const newCharacter: Character = new Character(playerName, characterName, characterClass, level, background, race, alignment );
+  console.log(newCharacter);
   }
-
 }
+
+
+  // constructor( public playerName, string, public characterName: string, public characterClass: number, public level: number, public background: string, public race: string, public alignment: string ) {}
