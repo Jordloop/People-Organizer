9f08a86e035f1f8649424f5094dc6d96647eebe9
JordLoop <JordLoop@gmail.com>
2017-07-14 11:01:25 -0700

    add character.service.ts

---
+++ src/app/character.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { Character } from './character.model';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+
+
+@Injectable()
+export class CharacterService {
+
+  constructor() { }
+
+}
--- src/app/characters/characters.component.ts
+++ src/app/characters/characters.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Character } from '../character.model';
+// import {  } from '';
+// import {  } from '';
+// import {  } from '';
+
+
+
 
 @Component({
   selector: 'app-characters',
