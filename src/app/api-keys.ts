ddd0d7d73865c3fd36ea86cdda660e7f9188c47c
JordLoop <JordLoop@gmail.com>
2017-07-14 10:12:40 -0700

    add character.model, and sample-character.model. add firebase to tsconfig and .gitignore

---
+++ src/app/api-keys.ts
@@ -0,0 +1,8 @@
+export var masterFirebaseConfig = {
+  apiKey: "AIzaSyB3fJ94QuM_EV9vWYX8tLkQd8uiH8Cx_0s",
+  authDomain: "characters-3ea48.firebaseapp.com",
+  databaseURL: "https://characters-3ea48.firebaseio.com",
+  projectId: "characters-3ea48",
+  storageBucket: "characters-3ea48.appspot.com",
+  messagingSenderId: "1061461397948"
+};
---
+++ src/app/character.model.ts
@@ -0,0 +1,3 @@
+export class Character {
+  constructor( public playerName: string, public characterName: string, public characterClass: number, public level: number, public background: string, public race: string, public alignment: string ) {}
+}
