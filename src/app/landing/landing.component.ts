0afa851cb81cfdd3d5e9a630e49ca3c301a08d4e
JordLoop <JordLoop@gmail.com>
2017-07-14 11:41:40 -0700

    add content to landing.component

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -5,11 +5,9 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.css']
 })
-export class LandingComponent implements OnInit {
+export class LandingComponent{
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  heading = "Organize your player's character information for D&D 5e."
+  content = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 
 }
