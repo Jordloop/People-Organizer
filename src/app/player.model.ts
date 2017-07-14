3a018cc103128a02335967608cd5ca1e3964cf21
JordLoop <JordLoop@gmail.com>
2017-07-14 09:42:47 -0700

    add character.model

---
+++ src/app/player.model.ts
@@ -0,0 +1,3 @@
+export class Character {
+  constructor( public playerName: string, public name: string, public characterClass: number, public level: number, public background: string, public race: string, public alignment: string ) {}
+}
